e7d9e33fb2ba93af3a89cd46c5337f38578fa551
= <=>
2018-12-28 14:41:11 +0100

    cleanup

---
+++ crawler.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = 'http://notfallplan-dortmund.notfallseelsorge-ekvw.de/?q=user%2Flogin&destination=rufbereitschaft'
+
+login_payload = {
+    'name': 'Pablo Gottheil',
+    'pass': 'Auf&290794',
+    # Preferably set your password in an env variable and sub it in.
+    'form_id': 'user_login'
+}
+
+r = requests.post(base_url, data=login_payload)
+
+soup = BeautifulSoup(r.text)
+
+spans_wo_class = soup.find_all("td", class_="")
+print(len(spans_wo_class))
+    
