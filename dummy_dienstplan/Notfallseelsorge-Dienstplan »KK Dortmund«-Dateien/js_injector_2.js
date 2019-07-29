742e7d75dd9e25dec08dbd194735b2851a80d984
Max Botschen <max.botschen@gmail.com>
2019-07-29 20:20:40 +0200

    dummy dienstplan hinzugefügt, laeuft noch nicht

---
+++ dummy_dienstplan/Notfallseelsorge-Dienstplan »KK Dortmund«-Dateien/js_injector_2.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.telinfo').hover(function(){
+        var title = $(this).attr('title');
+        $(this).data('tipText', title).removeAttr('title');
+        $('<p class="tooltip"></p>')
+        .text(title)
+        .appendTo('body')
+        .fadeIn('slow');
+    }, function() {
+        $(this).attr('title', $(this).data('tipText'));
+        $('.tooltip').remove();
+    }).mousemove(function(e) {
+        var mousex = e.pageX + 20;
+        var mousey = e.pageY + 20;
+        $('.tooltip')
+        .css({ top: mousey, left: mousex })
+    });
+});
