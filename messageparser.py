dd0b62738296f172ae69a2e75cb38c5a0e538246
nitzel <jan.s.92@web.de>
2019-08-04 22:52:05 +0100

    Added unittests for messageparser.py and refactored it

--- messageparser.py
+++ messageparser.py
@@ -1,12 +1,124 @@
 import re
 from typing import List, Set, Dict, Tuple, Optional
 import logging
+from enum import Enum
 
 regex_valid_time = r"\d{1,2}\:\d\d"
 regex_from_to = re.compile(f" (?P<from>{regex_valid_time}) (?:uhr)? - (?P<to>{regex_valid_time}) (?:uhr)?".replace(' ', r'\s*'))
 regex_from_or_to = re.compile(f" (?P<fromOrTo>(?:bis)|(?:ab)) (?P<time>{regex_valid_time})".replace(' ', r'\s*'))
 
-def parse(shift_start: str, shift_end: str, default_number: str, text_content: str):
+class FromOrTill(Enum):
+    FROM = 'ab'
+    TILL = 'bis'
+
+class TimeRule(object):
+    def __init__(self, time: str, from_or_till: FromOrTill, phone_number: str, pair_id: int = None, ):
+        self.time = time
+        self.from_or_till = from_or_till
+        self.pair_id = pair_id
+        self.phone_number = phone_number
+        # a comes before z, as FROM should be before TILL
+        self.sortable_key = f"{self.time}{'a' if self.from_or_till == FromOrTill.FROM else 'z'}"
+
+    def __repr__(self):
+        return f"""TimeRule(time="{self.time}", from_or_till="{self.from_or_till}", pair_id={self.pair_id}, phone_number="{self.phone_number}")"""
+
+    def __str__(self):
+        return f"[TimeRule<{self.from_or_till} {self.time}, pair={self.pair_id}, number={self.phone_number}>]"
+    
+    def __eq__(self, other) -> bool:
+        """
+        Returns true if other equals this rule in regards of pair id and phone number.
+        NB: If the `phone_number`s match and `pair_id`s are both None true is returned!
+        """
+        return (self.pair_id == other.pair_id) and (self.phone_number == other.phone_number)
+
+    @staticmethod
+    def sort(rules: List) -> List:
+        return sorted(rules, key = lambda rule: rule.sortable_key)
+
+def parse_rules_from_message(shift_start: str, shift_end: str, default_number: str, message: str):
+    logger = logging.getLogger("Parser")
+    logger.debug(f"Processing text '{message}'")
+
+    # pad all hours, 9:12 -> 09:12 
+    message = message.lower()
+    re.sub(r"(?<=\D)(\d:\d\d)", r"0\1", message)
+    rules: List[TimeRule] = []
+    pair_id = 1
+
+    for substitute in message.split(';'):
+        logger.debug(f"Processing substitute '{substitute}'")
+
+        numbers = re.findall(r"(?:\s)\+?\d{5,}", substitute)
+        if len(numbers) != 1:
+            logger.error(f"Found {len(numbers)} telephone numbers in substitute segment '{substitute}' but expected only one")
+            continue
+
+        number = numbers[0].strip()
+        logger.debug(f"  Found phone number '{number}' in substitute '{substitute}'")
+
+        for timeslot in re.split(r"(?:und)|,", substitute):
+            logger.debug(f"  Processing timeslot '{timeslot}'")
+
+            matches = regex_from_to.match(timeslot)
+            if matches:
+                # matches 12:30- 13:20 and 12:30 Uhr - 13:20 Uhr
+                time_from, time_till = matches.group('from'),  matches.group('to')
+                logger.debug(f"    Extracted timeslot '{time_from}' to '{time_till}'")
+                rules.append(TimeRule(time = time_from, from_or_till = FromOrTill.FROM, phone_number = number, pair_id = pair_id))
+                rules.append(TimeRule(time = time_till, from_or_till = FromOrTill.TILL, phone_number = number, pair_id = pair_id))
+                pair_id += 1
+                continue
+            
+            matches = regex_from_or_to.match(timeslot)
+            if matches:
+                # Matches 'ab 13:20' or 'bis 13:20'
+                prefix, time = matches.group('fromOrTo'), matches.group('time')
+                logger.debug(f"    Extracted timeslot '{prefix}' '{time}'")
+                if prefix == 'bis':
+                    logger.debug(f"    Equivalent to '{shift_start}' '{time}'")
+                    rules.append(TimeRule(time = shift_start, from_or_till = FromOrTill.FROM, phone_number = number))
+                    rules.append(TimeRule(time = time, from_or_till = FromOrTill.TILL, phone_number = number))
+                    continue
+                elif prefix == 'ab':
+                    logger.debug(f"    Equivalent to '{time}' '{shift_end}'")
+                    rules.append(TimeRule(time = time, from_or_till = FromOrTill.FROM, phone_number = number))
+                    rules.append(TimeRule(time = shift_end, from_or_till = FromOrTill.TILL, phone_number = number)) # superfluous
+                    continue
+                raise Exception(f"Failed to parse timeslot '{timeslot}', this should never happen")
+
+            logger.error(f"    Failed to parse timeslot '{timeslot}', make sure to use the format 'hh:mm - hh:mm' or 'ab hh:mm' or 'bis hh:mm'")
+
+    if not any(rule.time == shift_start for rule in rules):
+        rules.append({'time' : shift_start, 'prefix' : "ab", 'pair' : None, 'number' : default_number})
+
+    return TimeRule.sort(rules)
+
+def time_rules_to_interval_list(rules: List[TimeRule], default_number: str, shift_end: str):
+    # tricky: we have to translate the rules into a continuous list of intervals with the correct number for that interval
+    # If we encouter a substitution ending rule, we need to change back to the previous number, so we need a stack to keep
+    # track of the currently active numbers and take the most recent one of those
+
+    intervals = []
+    stack: List[TimeRule] = [TimeRule(None, None, default_number, None)] # [{'pair': None, 'number': default_number}]
+    for rule in rules:
+        if rule.from_or_till == FromOrTill.FROM: # FROm
+            stack.insert(0, rule)
+        else: # TILL
+            if rule in stack:
+                stack.remove(rule) # remove previously added 'ab' with same number & pair id
+
+        intervals.append([rule.time, stack[0].phone_number])
+
+    for i in range(len(intervals)-1):
+        intervals[i].insert(1, intervals[i+1][0])
+    intervals[-1].insert(1, shift_end)
+
+    no_empty_intervals = filter(lambda interval: interval[0] != interval[1], intervals)
+    return list(no_empty_intervals)
+
+def get_interval_list_from_message(shift_start: str, shift_end: str, default_number: str, message: str):
     """
     Returns a list of time intervals with the given phone number for the interval, according to a string with correct formatting
     all times are handled as "24:00" strings
@@ -24,94 +136,16 @@ def parse(shift_start: str, shift_end: str, default_number: str, text_content: s
 
     Parameters:
     shift_start (string): regular beginning of shift
-    shift_end   (string): regular end of shift
+    shift_end (string): regular end of shift
     default_number (string): regular phone number for this shift
-    text_content   (string): content of the comment box, which gets parsed into the substitution rules 
+    message (string): content of the comment box, which gets parsed into the substitution rules 
 
     Returns:
     intervals      (list of lists): list of lists with format [interval_start, interval_end, phone_number]
     """
 
-    logger = logging.getLogger("Parser")
-    logger.debug(f"Processing text '{text_content}'")
-
-    # pad all hours, 9:12 -> 09:12 
-    text_content = text_content.lower()
-    re.sub(r"(?<=\D)(\d:\d\d)", r"0\1", text_content)
-    rules = []
-    pair_id = 1
-
-    for substitute in text_content.split(';'):
-        logger.debug(f"Processing substitute '{substitute}'")
-
-        numbers = re.findall(r"(?:\s)\+?\d{5,}", substitute)
-        if len(numbers) != 1:
-            logger.error(f"Found {len(numbers)} telephone numbers in substitute segment '{substitute}' but expected only one")
-            continue
-
-        number = numbers[0]
-        logger.debug(f"  Found phone number '{number}' in substitute '{substitute}'")
-
-        for timeslot in re.split(r"(?:und)|,", substitute):
-            logger.debug(f"  Processing timeslot '{timeslot}'")
-
-            matches = regex_from_to.match(timeslot)
-            if matches:
-                # matches 12:30- 13:20 and 12:30 Uhr - 13:20 Uhr
-                time_from, time_to = matches.group('from'),  matches.group('to')
-                logger.debug(f"    Extracted timeslot '{time_from}' to '{time_to}'")
-                rules.append({'time' : time_from, 'prefix' : "ab", 'pair' : pair_id, 'number' : number})
-                rules.append({'time' : time_to, 'prefix': "bis", 'pair' : pair_id, 'number' : number})
-                pair_id += 1
-                continue
-
-            
-            matches = regex_from_or_to.match(timeslot)
-            if matches:
-                # Matches 'ab 13:20' or 'bis 13:20'
-                prefix, time = matches.group('fromOrTo'), matches.group('time')
-                logger.debug(f"    Extracted timeslot '{prefix}' '{time}'")
-                if prefix == 'bis':
-                    logger.debug(f"    Equivalent to '{shift_start}' '{time}'")
-                    rules.append({'time' : shift_start, 'prefix' : 'ab', 'pair': None, 'number' : number})
-                    rules.append({'time' : time, 'prefix' : 'bis', 'pair' : None, 'number' : number})
-                    continue
-                elif prefix == 'ab':
-                    logger.debug(f"    Equivalent to '{time}' '{shift_end}'")
-                    rules.append({'time' : time, 'prefix' : 'ab', 'pair': None, 'number' : number})
-                    # rules.append({'time' : shift_end, 'prefix' : 'bis', 'pair' : None, 'number' : number})
-                    continue
-                raise Exception(f"Failed to parse timeslot '{timeslot}', this should never happen")
-
-            logger.error(f"    Failed to parse timeslot '{timeslot}', make sure to use the format 'hh:mm - hh:mm' or 'ab hh:mm' or 'bis hh:mm'")
-
-    if not any(rule['time'] == shift_start for rule in rules):
-        rules.append({'time' : shift_start, 'prefix' : "ab", 'pair' : None, 'number' : default_number})
-
-    rules = sorted(rules, key=lambda d: d['time'] + d['prefix'])
-
-    # tricky: we have to translate the rules into a continuous list of intervals with the correct number for that interval
-    # If we encouter a substitution ending rule, we need to change back to the previous number, so we need a stack to keep
-    # track of the currently active numbers and take the most recent one of those
-
-    # stack.pop, stack.append
-    # stack.last()
-
-    intervals = []
-    stack = [{'pair': None, 'number': default_number}]
-    for rule in rules:
-        time = rule.pop('time')
-        if rule.pop('prefix') == 'ab':
-            stack.insert(0, rule)
-        else: # bis
-            if rule in stack:
-                stack.remove(rule) # remove previously added 'ab' with same number & pair id
-
-        intervals.append([time, stack[0]['number']])
-
-    for i in range(len(intervals)-1):
-        intervals[i].insert(1, intervals[i+1][0])
-    intervals[-1].insert(1, shift_end)
-
-    no_empty_intervals = filter(lambda interval: interval[0] != interval[1], intervals)
-    return list(no_empty_intervals)
+    rules = parse_rules_from_message(shift_start = shift_start,
+                                     shift_end = shift_end,
+                                     default_number = default_number,
+                                     message = message)
+    return time_rules_to_interval_list(rules, default_number, shift_end)
--- test_messageparser.py
+++ test_messageparser.py
@@ -1,26 +1,82 @@
 import pytest
 import messageparser
 
-def test_parsers_result_against_expected_shift_sequence():  
+def test_TimeRule_sort_GivenSameTime_SortsByFromOrTill_And_PersistsRelativeOrder():
+    rules = [
+        messageparser.TimeRule(time="08:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=1, phone_number="0"),
+        messageparser.TimeRule(time="08:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=2, phone_number="1"),
+        messageparser.TimeRule(time="08:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=3, phone_number="2"),
+    ]
+    expected_order = [rules[0], rules[2], rules[1]]
+    assert expected_order == messageparser.TimeRule.sort(rules)
+
+def test_TimeRule_sort_GivenDifferentTimes_SortsByTimeAndFromOrTill():
+    rules = [
+        messageparser.TimeRule(time="09:42", from_or_till=messageparser.FromOrTill.FROM, pair_id=1, phone_number="0"),
+        messageparser.TimeRule(time="09:37", from_or_till=messageparser.FromOrTill.TILL, pair_id=3, phone_number="1"),
+        messageparser.TimeRule(time="10:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=2, phone_number="2"),
+        messageparser.TimeRule(time="09:37", from_or_till=messageparser.FromOrTill.FROM, pair_id=3, phone_number="3"),
+    ]
+    expected_order = [rules[3], rules[1], rules[0], rules[2]]
+    assert expected_order == messageparser.TimeRule.sort(rules)
+
+def test_parse_rules_from_message():  
     shift_start = "08:00"
     shift_end   = "20:00"
     default_number = "+49767676"
 
-    test1 = "bis 12:00 Uhr +491231; ab 17:00 Uhr +467898; 13:30 Uhr - 14:15 Uhr, 14:30-14:45, ab 16:00 Uhr +472892; 16:00-16:20 +4934623"
+    test_message = "bis 12:00 Uhr +491231; ab 17:00 Uhr +467898; 13:30 Uhr - 14:15 Uhr, 14:30-14:45, ab 16:00 Uhr +472892; 16:00-16:20 +4934623"
 
-    expected_intervals = [
-        ['08:00', '12:00', ' +491231'],
-        ['12:00', '13:30', '+49767676'],
-        ['13:30', '14:15', ' +472892'],
-        ['14:15', '14:30', '+49767676'],
-        ['14:30', '14:45', ' +472892'],
-        ['14:45', '16:00', '+49767676'],
-        ['16:00', '16:20', ' +4934623'],
-        ['16:20', '17:00', ' +472892'],
-        ['17:00', '20:00', ' +467898']
+    expected_rules = [
+        messageparser.TimeRule(time="08:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=None, phone_number="+491231"),
+        messageparser.TimeRule(time="12:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=None, phone_number="+491231"),
+        messageparser.TimeRule(time="13:30", from_or_till=messageparser.FromOrTill.FROM, pair_id=1, phone_number="+472892"),
+        messageparser.TimeRule(time="14:15", from_or_till=messageparser.FromOrTill.TILL, pair_id=1, phone_number="+472892"),
+        messageparser.TimeRule(time="14:30", from_or_till=messageparser.FromOrTill.FROM, pair_id=2, phone_number="+472892"),
+        messageparser.TimeRule(time="14:45", from_or_till=messageparser.FromOrTill.TILL, pair_id=2, phone_number="+472892"),
+        messageparser.TimeRule(time="16:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=None, phone_number="+472892"),
+        messageparser.TimeRule(time="16:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=3, phone_number="+4934623"),
+        messageparser.TimeRule(time="16:20", from_or_till=messageparser.FromOrTill.TILL, pair_id=3, phone_number="+4934623"),
+        messageparser.TimeRule(time="17:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=None, phone_number="+467898"),
+        messageparser.TimeRule(time="20:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=None, phone_number="+467898"),
+        messageparser.TimeRule(time="20:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=None, phone_number="+472892"),
     ]
 
-    actual_intervals = messageparser.parse(shift_start, shift_end, default_number, test1)
+    actual_rules = messageparser.parse_rules_from_message(shift_start, shift_end, default_number, test_message)
+
+    assert expected_rules == actual_rules
+
+def test_time_rules_to_interval_list():  
+    shift_end = "20:00"
+    default_number = "+49767676"
+
+    rules = [
+        messageparser.TimeRule(time="08:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=None, phone_number="+491231"),
+        messageparser.TimeRule(time="12:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=None, phone_number="+491231"),
+        messageparser.TimeRule(time="13:30", from_or_till=messageparser.FromOrTill.FROM, pair_id=1, phone_number="+472892"),
+        messageparser.TimeRule(time="14:15", from_or_till=messageparser.FromOrTill.TILL, pair_id=1, phone_number="+472892"),
+        messageparser.TimeRule(time="14:30", from_or_till=messageparser.FromOrTill.FROM, pair_id=2, phone_number="+472892"),
+        messageparser.TimeRule(time="14:45", from_or_till=messageparser.FromOrTill.TILL, pair_id=2, phone_number="+472892"),
+        messageparser.TimeRule(time="16:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=None, phone_number="+472892"),
+        messageparser.TimeRule(time="16:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=3, phone_number="+4934623"),
+        messageparser.TimeRule(time="16:20", from_or_till=messageparser.FromOrTill.TILL, pair_id=3, phone_number="+4934623"),
+        messageparser.TimeRule(time="17:00", from_or_till=messageparser.FromOrTill.FROM, pair_id=None, phone_number="+467898"),
+        messageparser.TimeRule(time="20:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=None, phone_number="+467898"),
+        messageparser.TimeRule(time="20:00", from_or_till=messageparser.FromOrTill.TILL, pair_id=None, phone_number="+472892"),
+    ]
+
+    expected_intervals = [
+        ['08:00', '12:00', '+491231'],
+        ['12:00', '13:30', '+49767676'],
+        ['13:30', '14:15', '+472892'],
+        ['14:15', '14:30', '+49767676'],
+        ['14:30', '14:45', '+472892'],
+        ['14:45', '16:00', '+49767676'],
+        ['16:00', '16:20', '+4934623'],
+        ['16:20', '17:00', '+472892'],
+        ['17:00', '20:00', '+467898']
+    ]
+
+    actual_intervals = messageparser.time_rules_to_interval_list(rules, default_number, shift_end)
     
     assert expected_intervals == actual_intervals
-    # self.assertSequenceEqual(expected_intervals, actual_intervals)
