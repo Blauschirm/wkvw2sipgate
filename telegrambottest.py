a827faa37f7a346847db046d74d67ee78773a0f8
Max Botschen <max.botschen@gmail.com>
2019-09-29 22:58:02 +0200

    telegrambot can provice tables. its so pretty

--- telegrambottest.py
+++ telegrambottest.py
@@ -1,8 +1,10 @@
 from telegram.ext import Updater, CommandHandler, Filters, CallbackContext, MessageHandler
-from telegram import Update
+from telegram import Update, Message, Bot, ParseMode
 import logging, json
 from threading import Thread, Event
-from crawl import get_month
+from crawl import get_month, TimeSlot
+from typing import List, Set, Dict, Tuple, Optional
+from math import ceil, floor
 
 class IntervalThread(Thread):
     def __init__(self, timeout, callback):
@@ -53,23 +55,29 @@ def status(update: Update, context: CallbackContext):
     global TESTING
     update.message.reply_text(f"Der Bot läuft (yay!) {'im Testmodus' if TESTING else 'normal'}. Registered chats: {registered_chat_ids}")
 
-def message_everybody():
-    global updater
+def message_everybody(bot: Bot):
     print("MESSAGING EVERYBODY!")
     for id in registered_chat_ids:
         try:
-            updater.bot.send_message(chat_id=id, text="1 piep piep piep, ich hab euch alle lieb!")
+            bot.send_message(chat_id=id, text="1 piep piep piep, ich hab euch alle lieb!")
         except Exception as err:
             print("Des hat ned 'klappt", err)
 
 def return_day(update: Update, context: CallbackContext):
-    day_id = int(context.args[0], 10) - 1
-    day_data = get_month()[day_id]
+    day_id = int(context.args[0], 10) - 1 if len(context.args) > 0 else 0
+    day_data: List[TimeSlot] = get_month()[day_id]
     reply_text = f"Fuer den {day_id + 1}.:\n"
     for shift_id, shift_name in enumerate(["NSF1", "NSFW2", "Dude?"]):
-        reply_text += f"**Gruppe {shift_name}**\n"
-        reply_text += day_data[shift_id]+"\n"
-    update.message.reply_text(reply_text)
+        max_phone_number_length = max(map(lambda slot: len(slot.phone_number), day_data[shift_id]), default=5)
+        print("max phone number len", max_phone_number_length)
+        reply_text += f"*Gruppe {shift_name}*\n"
+        reply_text += "```\n" # Telegram does not currently support Markdown tables so have to use verbatim
+        reply_text += f"|Start| End |{' ' * ceil((max_phone_number_length-5)/2.)}Phone{' ' * floor((max_phone_number_length-5)/2)}|\n"
+        reply_text += f"|-----|-----|{'-' * max_phone_number_length}|\n"
+        for slot in day_data[shift_id]:
+            reply_text += f"|{slot.start_time}|{slot.end_time}|{slot.phone_number:{max_phone_number_length}}|\n"
+        reply_text += "```\n"
+    update.message.reply_markdown(reply_text)
 
 def unknown_command(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
@@ -92,7 +100,7 @@ updater.dispatcher.add_handler(CommandHandler('status', status))
 updater.dispatcher.add_handler(CommandHandler('day', return_day))
 updater.dispatcher.add_handler(MessageHandler(Filters._Command(), unknown_command))
 
-t = IntervalThread(60, message_everybody)
+t = IntervalThread(60, lambda: message_everybody(updater.bot))
 t.start()
 
 try:
